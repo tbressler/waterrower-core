dbf3d49885a4daff4e86dd783dfb68281b1daffb
tbressler <android.tbressler@gmail.com>
2017-09-03 12:24:26 +0200

    Added method to receive the serial ports from the operating system.

--- src/test/java/de/tbressler/waterrower/io/TestRxTxCommunicationService.java
+++ src/test/java/de/tbressler/waterrower/io/TestRxTxCommunicationService.java
@@ -1,8 +1,34 @@
 package de.tbressler.waterrower.io;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
+ * Tests for class RxTxCommunicationService.
+ *
  * @author Tobias Bressler
  * @version 1.0
  */
 public class TestRxTxCommunicationService {
+
+    /* Class under test. */
+    private RxTxCommunicationService rxTxCommunicationService;
+
+
+    @Before
+    public void setUp() {
+        rxTxCommunicationService = new RxTxCommunicationService();
+    }
+
+
+    /**
+     * Checks if getSerialPorts returns a non null list.
+     */
+    @Test
+    public void getSerialPorts_returnsNonNull() {
+        assertTrue(rxTxCommunicationService.getSerialPorts() != null);
+    }
+
 }
