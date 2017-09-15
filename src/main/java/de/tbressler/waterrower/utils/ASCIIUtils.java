2d492a2ed20f722a07a9949f8b7826cdee7211c0
tbressler <android.tbressler@gmail.com>
2017-09-15 20:20:44 +0200

    Added utils for conversion of ACD values.

--- src/main/java/de/tbressler/waterrower/utils/ASCIIUtils.java
+++ src/main/java/de/tbressler/waterrower/utils/ASCIIUtils.java
@@ -1,13 +1,51 @@
 package de.tbressler.waterrower.utils;
 
+import static java.lang.String.valueOf;
+import static java.util.Objects.requireNonNull;
+
 /**
  * Utils for conversion of ASCII data.
+ * The class supports:
+ * - ACD
  *
  * @author Tobias Bressler
  * @version 1.0
  */
 public class ASCIIUtils {
 
-    
+    /* Private constructor. */
+    private ASCIIUtils() {}
+
+    /**
+     * Returns the ACD (ASCII coded decimal) as int value.
+     *
+     * @param ascii The ACD value, must not be null.
+     * @return The integer value.
+     *
+     * @throws NumberFormatException If not a decimal value.
+     */
+    public static int acdToInt(String ascii) throws NumberFormatException {
+        return new Integer(requireNonNull(ascii));
+    }
+
+
+    /**
+     * Returns the int value as ACD (ASCII coded decimal).
+     *
+     * @param value The int value.
+     * @param chars The number of chars.
+     * @return The int value as ACD.
+     *
+     * @throws NumberFormatException If the int has more characters than the number of chars given.
+     */
+    public static String intToAcd(int value, int chars) throws NumberFormatException {
+        String ascii = valueOf(value);
+        int numberOfLeadingZeros = chars - ascii.length();
+        if (numberOfLeadingZeros < 0)
+            throw new NumberFormatException("Number '"+value+"' has more than "+chars+" characters!");
+        for(int i=0; i<numberOfLeadingZeros; i++)
+            ascii = "0"+ascii;
+        return ascii;
+    }
 
 }
---
+++ src/test/java/de/tbressler/waterrower/utils/TestASCIIUtils.java
@@ -0,0 +1,83 @@
+package de.tbressler.waterrower.utils;
+
+import org.junit.Test;
+
+import static de.tbressler.waterrower.utils.ASCIIUtils.acdToInt;
+import static de.tbressler.waterrower.utils.ASCIIUtils.intToAcd;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for class ASCIIUtils.
+ *
+ * @author Tobias Bressler
+ * @version 1.0
+ */
+public class TestASCIIUtils {
+
+    @Test(expected = NullPointerException.class)
+    public void acdToInt_withNull_throwsException() {
+        acdToInt(null);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void acdToInt_withText_throwsNumberFormatException() {
+        acdToInt("Text");
+    }
+
+    @Test
+    public void acdToInt_withDecimal1_returnsInteger1() {
+        assertEquals(1, acdToInt("1"));
+    }
+
+    @Test
+    public void acdToInt_withDecimal123_returnsInteger123() {
+        assertEquals(123, acdToInt("123"));
+    }
+
+    @Test
+    public void acdToInt_withDecimal101_returnsInteger101() {
+        assertEquals(101, acdToInt("101"));
+    }
+
+    @Test
+    public void acdToInt_withDecimal001_returnsInteger1() {
+        assertEquals(1, acdToInt("001"));
+    }
+
+
+    @Test
+    public void intToAcd_with0ValueAnd1Chars_returnsString0() {
+        assertEquals("0", intToAcd(0, 1));
+    }
+
+    @Test
+    public void intToAcd_with2ValueAnd1Chars_returnsString2() {
+        assertEquals("2", intToAcd(2, 1));
+    }
+
+    @Test
+    public void intToAcd_with123ValueAnd3Chars_returnsString123() {
+        assertEquals("123", intToAcd(123, 3));
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void intToAcd_with123ValueAnd2Chars_throwsNumberFormatException() {
+        intToAcd(123, 2);
+    }
+
+    @Test
+    public void intToAcd_with1And3Chars_returnsString001() {
+        assertEquals("001", intToAcd(1, 3));
+    }
+
+    @Test
+    public void intToAcd_with1And2Chars_returnsString01() {
+        assertEquals("01", intToAcd(1, 2));
+    }
+
+    @Test
+    public void intToAcd_with12And3Chars_returnsString012() {
+        assertEquals("012", intToAcd(12, 3));
+    }
+
+}
