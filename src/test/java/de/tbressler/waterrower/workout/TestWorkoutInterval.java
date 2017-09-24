caeceeafd007496ff795452e7f4805baf3acbb0d
tbressler <android.tbressler@gmail.com>
2017-09-24 10:35:55 +0200

    Clean imports.

--- src/test/java/de/tbressler/waterrower/workout/TestWorkoutInterval.java
+++ src/test/java/de/tbressler/waterrower/workout/TestWorkoutInterval.java
@@ -4,7 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static de.tbressler.waterrower.workout.WorkoutUnit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for class WorkoutInterval.
