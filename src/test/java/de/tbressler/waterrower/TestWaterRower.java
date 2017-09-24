1bfa9bd4e95b4d36d9ca89ee91cc1584b9e29351
tbressler <android.tbressler@gmail.com>
2017-09-24 17:20:00 +0200

    Added test class for WaterRower.

---
+++ src/test/java/de/tbressler/waterrower/TestWaterRower.java
@@ -0,0 +1,98 @@
+package de.tbressler.waterrower;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for class WaterRower.
+ *
+ * @author Tobias Bressler
+ * @version 1.0
+ */
+public class TestWaterRower {
+
+    // Class under test.
+    private WaterRower waterRower;
+
+    // Mocks:
+
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+    // Constructor:
+
+    @Test
+    public void new_() {
+
+    }
+
+
+    // Connect / disconnect:
+
+    @Test
+    public void connect() throws Exception {
+
+    }
+
+    @Test
+    public void isConnected() throws Exception {
+
+    }
+
+    @Test
+    public void disconnect() throws Exception {
+
+    }
+
+
+    // Interaction:
+
+    @Test
+    public void performReset() throws Exception {
+
+    }
+
+    @Test
+    public void startWorkout() throws Exception {
+
+    }
+
+
+    // Subscribe / unsubscribe:
+
+    @Test
+    public void subscribe() throws Exception {
+
+    }
+
+    @Test
+    public void unsubscribe() throws Exception {
+
+    }
+
+    // Watchdogs:
+
+
+
+
+    // Listeners:
+
+    @Test
+    public void addConnectionListener() throws Exception {
+
+    }
+
+    @Test
+    public void removeConnectionListener() throws Exception {
+
+    }
+
+}
