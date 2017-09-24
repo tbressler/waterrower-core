2be683570b58d8cdb3c188851cf6b4918ef8191d
tbressler <android.tbressler@gmail.com>
2017-09-24 16:00:37 +0200

    New WaterRower class implemented.

--- src/main/java/de/tbressler/waterrower/WaterRowerV2.java
+++ src/main/java/de/tbressler/waterrower/WaterRowerV2.java
@@ -2,20 +2,33 @@ package de.tbressler.waterrower;
 
 import de.tbressler.waterrower.io.IRxtxConnectionListener;
 import de.tbressler.waterrower.io.RxtxConnectionListener;
+import de.tbressler.waterrower.io.msg.AbstractMessage;
+import de.tbressler.waterrower.io.msg.in.ErrorMessage;
+import de.tbressler.waterrower.io.msg.in.HardwareTypeMessage;
+import de.tbressler.waterrower.io.msg.in.ModelInformationMessage;
 import de.tbressler.waterrower.io.msg.out.ExitCommunicationMessage;
+import de.tbressler.waterrower.io.msg.out.RequestModelInformationMessage;
 import de.tbressler.waterrower.io.msg.out.ResetMessage;
+import de.tbressler.waterrower.io.msg.out.StartCommunicationMessage;
+import de.tbressler.waterrower.log.Log;
 import de.tbressler.waterrower.model.ErrorCode;
 import de.tbressler.waterrower.model.ModelInformation;
 import de.tbressler.waterrower.subscriptions.ISubscription;
 import de.tbressler.waterrower.subscriptions.SubscriptionPollingService;
+import de.tbressler.waterrower.watchdog.DeviceVerificationWatchdog;
+import de.tbressler.waterrower.watchdog.ITimeoutListener;
+import de.tbressler.waterrower.watchdog.PingWatchdog;
 import de.tbressler.waterrower.workout.Workout;
 import io.netty.channel.rxtx.RxtxDeviceAddress;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ScheduledExecutorService;
 
+import static de.tbressler.waterrower.log.Log.LIBRARY;
+import static de.tbressler.waterrower.model.ErrorCode.*;
+import static de.tbressler.waterrower.utils.WaterRowerCompatibility.isSupportedWaterRower;
+import static de.tbressler.waterrower.watchdog.TimeoutReason.DEVICE_NOT_CONFIRMED_TIMEOUT;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -29,11 +42,18 @@ import static java.util.Objects.requireNonNull;
  */
 public class WaterRowerV2 {
 
+    /* Handles the connection to the WaterRower. */
     private final WaterRowerConnector connector;
 
+    /* Polls and handles subscriptions. */
     private final SubscriptionPollingService subscriptionPollingService;
 
-    private final ScheduledExecutorService executorService;
+    /* Watchdog that checks if a ping is received periodically. */
+    private final PingWatchdog pingWatchdog;
+
+    /* Watchdog that checks if the device sends it's model information in order to verify
+     * compatibility with the library. */
+    private final DeviceVerificationWatchdog deviceVerificationWatchdog;
 
     /* All listeners. */
     private List<IWaterRowerConnectionListener> listeners = new ArrayList<>();
@@ -42,19 +62,65 @@ public class WaterRowerV2 {
     /* The listener for the WaterRower connector. */
     private IRxtxConnectionListener connectionListener = new RxtxConnectionListener() {
 
+        @Override
+        public void onConnected() {
+            handleOnConnect();
+        }
+
+        @Override
+        public void onMessageReceived(AbstractMessage msg) {
+            handleOnMessageReceived(msg);
+        }
+
+        @Override
+        public void onDisconnected() {
+            handleOnDisconnected();
+        }
+
+        @Override
+        public void onError() {
+            fireOnError(COMMUNICATION_FAILED);
+        }
+
     };
 
 
+    /* Listener for watchdog timeouts. */
+    private ITimeoutListener timeoutListener = reason -> {
+        fireOnError((reason == DEVICE_NOT_CONFIRMED_TIMEOUT) ? DEVICE_NOT_SUPPORTED : TIMEOUT);
+    };
+
+
+    /**
+     * The entry point of the WaterRower library.
+     *
+     * This class connects with the WaterRower and exchanges the information between PC and
+     * WaterRower monitor.
+     *
+     * @param connector The connector to the WaterRower, must not be null.
+     * @param pingWatchdog The Watchdog that checks if a ping is received periodically, must not
+     *                     be null.
+     * @param deviceVerificationWatchdog Watchdog that checks if the device sends it's model
+     *                                   information in order to verify compatibility with the
+     *                                   library. Must not be null.
+     * @param subscriptionPollingService The subscription polling service, which polls and
+     *                                   handles the subscriptions. Must not be null.
+     */
     public WaterRowerV2(WaterRowerConnector connector,
-                        SubscriptionPollingService subscriptionPollingService,
-                        ScheduledExecutorService executorService) {
+                        PingWatchdog pingWatchdog,
+                        DeviceVerificationWatchdog deviceVerificationWatchdog,
+                        SubscriptionPollingService subscriptionPollingService) {
 
         this.connector = requireNonNull(connector);
         this.connector.addConnectionListener(connectionListener);
 
-        this.subscriptionPollingService = requireNonNull(subscriptionPollingService);
+        this.pingWatchdog = pingWatchdog;
+        this.pingWatchdog.setTimeoutListener(timeoutListener);
 
-        this.executorService = requireNonNull(executorService);
+        this.deviceVerificationWatchdog = deviceVerificationWatchdog;
+        this.deviceVerificationWatchdog.setTimeoutListener(timeoutListener);
+
+        this.subscriptionPollingService = requireNonNull(subscriptionPollingService);
     }
 
 
@@ -66,7 +132,94 @@ public class WaterRowerV2 {
      * @throws IOException If connect fails.
      */
     public void connect(RxtxDeviceAddress address) throws IOException {
+        if (connector.isConnected())
+            throw new IOException("Already connected! Can not connect again.");
         connector.connect(requireNonNull(address));
+    }
+
+
+    /* Handles the task after a successful connect. */
+    private void handleOnConnect() {
+        try {
+
+            Log.debug(LIBRARY, "RXTX connected. Sending 'start communication' message.");
+
+            deviceVerificationWatchdog.start();
+
+            connector.send(new StartCommunicationMessage());
+
+        } catch (IOException e) {
+            Log.error("Couldn't send 'start communication' message!", e);
+            fireOnError(COMMUNICATION_FAILED);
+        }
+    }
+
+
+    /* Handles received messages. */
+    private void handleOnMessageReceived(AbstractMessage msg) {
+        try {
+
+            pingWatchdog.pingReceived();
+
+            handleLowLevelMessages(msg);
+
+        } catch (IOException e) {
+            Log.error("A communication error occurred!", e);
+            fireOnError(COMMUNICATION_FAILED);
+        }
+    }
+
+    /* Handles all the low level messages like ping, hardware type, ... */
+    private void handleLowLevelMessages(AbstractMessage msg) throws IOException {
+
+        if (msg instanceof HardwareTypeMessage) {
+
+            if (((HardwareTypeMessage) msg).isWaterRower()) {
+
+                Log.debug(LIBRARY, "Connected with WaterRower. Sending poll for model information.");
+
+                connector.send(new RequestModelInformationMessage());
+
+            } else {
+
+                Log.warn(LIBRARY, "The connected device is not a WaterRower!");
+
+                fireOnError(DEVICE_NOT_SUPPORTED);
+            }
+
+        } else if (msg instanceof ModelInformationMessage) {
+
+            ModelInformation modelInformation = ((ModelInformationMessage) msg).getModelInformation();
+
+            Log.debug(LIBRARY, "Received model information from connected WaterRower:\n" +
+                    " Model: " + modelInformation);
+
+            if (isSupportedWaterRower(modelInformation)) {
+
+                Log.debug(LIBRARY, "Monitor type and firmware are supported by this library. Successfully connected with WaterRower.");
+
+                // Set device model confirmed and stop watchdog:
+                deviceVerificationWatchdog.setDeviceConfirmed(true);
+                deviceVerificationWatchdog.stop();
+
+                // Start ping watchdog.
+                pingWatchdog.start();
+
+                fireOnConnected(modelInformation);
+
+            } else {
+
+                Log.warn(LIBRARY, "The monitor type and/or firmware of the connected WaterRower are not supported by this library!");
+
+                fireOnError(DEVICE_NOT_SUPPORTED);
+            }
+
+        } else if (msg instanceof ErrorMessage) {
+
+            Log.debug(LIBRARY, "Error message received from WaterRower monitor.");
+
+            fireOnError(ERROR_MESSAGE_RECEIVED);
+        }
     }
 
 
@@ -76,8 +229,9 @@ public class WaterRowerV2 {
      * @return True if connected to WaterRower monitor.
      */
     public boolean isConnected() {
-
-        return false;
+        if (!connector.isConnected())
+            return false;
+        return deviceVerificationWatchdog.isDeviceConfirmed();
     }
 
 
@@ -87,10 +241,24 @@ public class WaterRowerV2 {
      * @throws IOException If disconnect fails.
      */
     public void disconnect() throws IOException {
+        checkIfConnected();
+
         // Be polite and send a goodbye.
         connector.send(new ExitCommunicationMessage());
         // Disconnect.
         connector.disconnect();
+    }
+
+
+    /* Handle disconnect. */
+    private void handleOnDisconnected() {
+
+        Log.debug(LIBRARY, "RXTX disconnected.");
+
+        deviceVerificationWatchdog.stop();
+        pingWatchdog.stop();
+
+        fireOnDisconnected();
     }
 
 
@@ -99,7 +267,7 @@ public class WaterRowerV2 {
      * power button. Used prior to configuring the rowing computer from a PC. Interactive mode will be disabled on a
      * reset.
      *
-     * @throws IOException If message couldn't be send.
+     * @throws IOException If the message couldn't be send.
      */
     public void performReset() throws IOException {
         checkIfConnected();
