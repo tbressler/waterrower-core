639b2702eaed9f5b202378cfa68015d995fe57ac
tbressler <android.tbressler@gmail.com>
2017-10-01 12:47:09 +0200

    Added more tests.

---
+++ src/test/java/de/tbressler/waterrower/subscriptions/TestDebugSubscription.java
@@ -0,0 +1,56 @@
+package de.tbressler.waterrower.subscriptions;
+
+import de.tbressler.waterrower.io.msg.in.DataMemoryMessage;
+import de.tbressler.waterrower.io.msg.out.ReadMemoryMessage;
+import org.junit.Test;
+
+import static de.tbressler.waterrower.io.msg.Memory.DOUBLE_MEMORY;
+import static de.tbressler.waterrower.io.msg.Memory.SINGLE_MEMORY;
+import static de.tbressler.waterrower.model.MemoryLocation.CLOCK_DOWN_DEC;
+import static de.tbressler.waterrower.model.MemoryLocation.M_S_LOW_AVERAGE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests for class DebugSubscription.
+ *
+ * @author Tobias Bressler
+ * @version 1.0
+ */
+public class TestDebugSubscription {
+
+    // Constructor:
+
+    @Test(expected = NullPointerException.class)
+    public void new_withNullMemory_throwsNPE() {
+        new DebugSubscription(null, CLOCK_DOWN_DEC) {
+            @Override
+            void handle(DataMemoryMessage msg) {}
+        };
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void new_withNullMemoryLocation_throwsNPE() {
+        new DebugSubscription(SINGLE_MEMORY, null) {
+            @Override
+            void handle(DataMemoryMessage msg) {}
+        };
+    }
+
+    // Poll:
+
+    @Test
+    public void poll_returnsMessageForConstructorValues() {
+        DebugSubscription subscription = new DebugSubscription(DOUBLE_MEMORY, M_S_LOW_AVERAGE) {
+            @Override
+            void handle(DataMemoryMessage msg) {}
+        };
+
+        ReadMemoryMessage msg = (ReadMemoryMessage) subscription.poll();
+
+        assertNotNull(msg);
+        assertEquals(DOUBLE_MEMORY, msg.getMemory());
+        assertEquals(M_S_LOW_AVERAGE.getLocation(), msg.getLocation());
+    }
+
+}
